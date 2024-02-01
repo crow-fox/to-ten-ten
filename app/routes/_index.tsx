f395f0a824a34184fb1087dcb92013633555847f
crow-fox <surakamiya198@gmail.com>
2024-02-02 08:15:12 +0900

    画面に画像を取り込んで表示

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { ChangeEvent, useRef } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,25 +9,79 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files === null) {
+      return;
+    }
+
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const img = new Image();
+      // img.src = event.target.result as string;
+      img.onload = () => {
+        const canvas = canvasRef.current;
+        if (canvas === null) {
+          return;
+        }
+
+        const ctx = canvas.getContext("2d");
+        if (ctx === null) {
+          return;
+        }
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+        convertToDotImage(ctx, img.width, img.height);
+      };
+      img.src = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/future/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <label>
+        画像のアップロード
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          onChange={handleChange}
+        />
+      </label>
+
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
+}
+
+function convertToDotImage(
+  ctx: CanvasRenderingContext2D,
+  width: number,
+  height: number
+) {
+  const imageData = ctx.getImageData(0, 0, width, height);
+  const data = imageData.data;
+
+  // ドットのサイズ（ここを調整してドットの大きさを変えられます）
+  const dotSize = 24;
+
+  for (let y = 0; y < height; y += dotSize) {
+    for (let x = 0; x < width; x += dotSize) {
+      const i = (y * width + x) * 4;
+      const r = data[i];
+      const g = data[i + 1];
+      const b = data[i + 2];
+      const a = data[i + 3];
+
+      // 各ドットに色を設定
+      ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
+      ctx.fillRect(x, y, dotSize, dotSize);
+    }
+  }
 }
