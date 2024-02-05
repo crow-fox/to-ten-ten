4c3650784509df5fc811b2d4926c11c43be9f7d5
crow-fox <surakamiya198@gmail.com>
2024-02-05 20:38:16 +0900

    画像をcanvasに表示

--- app/components/Canvas.tsx
+++ app/components/Canvas.tsx
@@ -1,28 +1,62 @@
-import { useEffect, useRef } from "react";
+import { ChangeEvent, useId, useRef } from "react";
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const uid = useId();
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas === null) return;
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    if (!file) return;
 
-    const ctx = canvas.getContext("2d");
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
 
-    if (ctx === null) {
-      console.log("Canvas サポートされていません");
-      return;
-    }
-  }, []);
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        const ctx = canvas.getContext("2d");
+        if (!ctx) return;
+
+        ctx.drawImage(img, 0, 0);
+
+        /// ドット絵に変換する
+        changeDotCanvasImage(ctx);
+      };
+      img.src = e.target?.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
 
   return (
-    <canvas
-      width={300}
-      height={300}
-      ref={canvasRef}
-      className="border border-slate-500 "
-    >
-      キャンバス
-    </canvas>
+    <div>
+      <p className="grid">
+        <label htmlFor={uid}>画像の入力</label>
+        <input
+          type="file"
+          id={uid}
+          onChange={handleChange}
+          className=" file:bg-slate-500 file:border-0 file:p-4 file:text-white file:rounded-full file:font-bold file:grid"
+        />
+      </p>
+      <canvas
+        // width={300}
+        // height={300}
+        ref={canvasRef}
+        className="border border-slate-500 border-dashed"
+      >
+        キャンバス
+      </canvas>
+    </div>
   );
+}
+
+function changeDotCanvasImage(ctx: CanvasRenderingContext2D) {
+  // canvasに表示されている画像をドット絵に変換する
+  const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  console.log(imageData.data);
 }
