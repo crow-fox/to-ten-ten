81916ec53d7d47bc5b1eaf5a1b4f6810fc034095
crow-fox <surakamiya198@gmail.com>
2024-02-21 19:59:11 +0900

    global styleはrootでのみ読み込む

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,9 @@
 import Canvas from "~/components/Canvas";
-import "../styles/globals.css";
 
 export default function IndexPage() {
   return (
     <div className="grid  gap-y-4 justify-items-center ">
-      <h1 className="font-bold text-2xl">Canvas</h1>
+      <h1 className="font-bold text-2xl text-red-500">Canvas</h1>
       <Canvas />
     </div>
   );
