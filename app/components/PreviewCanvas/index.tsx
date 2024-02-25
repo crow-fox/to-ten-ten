53aeec54af84883a5090fda3d838807e645866ae
crow-fox <surakamiya198@gmail.com>
2024-02-25 15:27:50 +0900

    PreviewCanvas

---
+++ app/components/PreviewCanvas/index.tsx
@@ -0,0 +1,44 @@
+import { RefObject } from "react";
+import { DotSize } from "~/components/DotSizeSelect/utils";
+import { Pixel2D, getPixelsSize } from "~/utils/canvas/pixels";
+
+type Props = {
+  myRef: RefObject<HTMLCanvasElement>;
+  pixels?: Pixel2D;
+  dotSize: DotSize;
+};
+
+export default function PreviewCanvas(props: Props) {
+  return (
+    <canvas
+      ref={props.myRef}
+      style={
+        props.pixels
+          ? {
+              width: "auto",
+              maxWidth: "100%",
+              height: "auto",
+              maxHeight: "512px",
+              aspectRatio: `${getPixelsSize(props.pixels).width}/${getPixelsSize(props.pixels).height}`,
+              background: "none",
+            }
+          : {
+              backgroundSize: `${props.dotSize}px ${props.dotSize}px`,
+              backgroundPosition: `0 0, ${props.dotSize / 2}px ${props.dotSize / 2}px`,
+            }
+      }
+      className="
+              h-[512px]
+              w-full
+              border
+              border-dashed
+              border-secondary-border
+              [background-image:repeating-linear-gradient(45deg,theme(colors.primary.background)_25%,transparent_25%,transparent_75%,theme(colors.primary.background)_75%,theme(colors.primary.background)),repeating-linear-gradient(45deg,theme(colors.primary.background)_25%,theme(colors.secondary.background)_25%,theme(colors.secondary.background)_75%,theme(colors.primary.background)_75%,theme(colors.primary.background))]
+              "
+    >
+      {props.pixels
+        ? "ドットに変換された画像"
+        : "まだ画像がアップロードされていません"}
+    </canvas>
+  );
+}
