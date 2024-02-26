77123de366ebfca9339e722d894c3f507e274e53
crow-fox <surakamiya198@gmail.com>
2024-02-26 09:49:51 +0900

    Dialogの外側をクリックした時に閉じる処理を追加

--- app/components/Dialog/index.tsx
+++ app/components/Dialog/index.tsx
@@ -2,7 +2,7 @@ import { ComponentProps, ReactNode, useRef } from "react";
 
 type DialogProps = ComponentProps<"dialog">;
 
-type Props = Omit<DialogProps, "children"> & {
+type Props = Omit<DialogProps, "children" | "onClick"> & {
   openButton: (open: () => void) => ReactNode;
   children: (close: () => void) => ReactNode;
 };
@@ -31,14 +31,24 @@ export default function Dialog({
     dialog.close();
   }
 
+  function handleOutsideClick(event: React.MouseEvent<HTMLDialogElement>) {
+    if (event.target === event.currentTarget) {
+      handleClose();
+    }
+  }
+
   return (
     <>
       {openButton(handleOpen)}
+      {/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
       <dialog
         {...dialogProps}
+        onClick={handleOutsideClick}
         ref={dialogRef}
         className="  fixed  inset-0 size-full max-h-full max-w-full overflow-auto bg-[transparent] backdrop:bg-[rgba(0,0,0,0.2)] open:grid"
       >
+        {/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+
         <div className={className}>{children(handleClose)}</div>
       </dialog>
     </>
