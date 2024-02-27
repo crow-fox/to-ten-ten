55ac115cef73e2f48528f7a0a0e40477a08b1186
crow-fox <surakamiya198@gmail.com>
2024-02-27 21:17:27 +0900

    _indexページ

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,13 @@
 import { MetaFunction } from "@remix-run/react";
+import { useState } from "react";
+import DotColorSelect from "~/components/DotColorSelect";
+import { DotColor } from "~/components/DotColorSelect/utils";
+import DotSizeSelect from "~/components/DotSizeSelect";
+import { DotSize } from "~/components/DotSizeSelect/utils";
+import ImageUpload from "~/components/ImageUpload";
+import OriginArea from "~/components/OriginArea";
+import PreviewArea from "~/components/PreviewArea";
+import Tabs from "~/components/Tabs";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,5 +19,62 @@ export const meta: MetaFunction = () => {
 };
 
 export default function IndexPage() {
-  return <h1>To Ten Ten</h1>;
+  const [originImage, setOriginImage] = useState<HTMLImageElement | undefined>(
+    undefined,
+  );
+  const [dotSize, setDotSize] = useState<DotSize>(32);
+  const [dotColor, setDotColor] = useState<DotColor>("full");
+
+  function handleUploaded(img: HTMLImageElement) {
+    setOriginImage(img);
+  }
+
+  function handleSelectDotSize(size: DotSize) {
+    setDotSize(size);
+  }
+
+  function handleSelectDotColor(color: DotColor) {
+    setDotColor(color);
+  }
+
+  return (
+    <div className=" space-y-20">
+      <div className=" mx-auto box-content grid max-w-576 gap-y-20 p-20">
+        <h2 className=" sr-only">ダッシュボード</h2>
+        <ImageUpload onUploaded={handleUploaded} />
+        <div className=" flex flex-wrap gap-20">
+          <DotSizeSelect value={dotSize} onChange={handleSelectDotSize} />
+          <DotColorSelect value={dotColor} onChange={handleSelectDotColor} />
+        </div>
+      </div>
+
+      <Tabs
+        items={[
+          {
+            label: "プレビュー",
+            content: (
+              <>
+                <h2 className="sr-only">プレビュー</h2>
+                <PreviewArea
+                  dotColor={dotColor}
+                  dotSize={dotSize}
+                  originImage={originImage}
+                />
+              </>
+            ),
+          },
+          {
+            label: "もとの画像",
+            content: (
+              <>
+                <h2 className="sr-only">もとの画像</h2>
+                <OriginArea dotSize={dotSize} originImage={originImage} />
+              </>
+            ),
+          },
+        ]}
+        navLabel="Before Afterの切り替え"
+      />
+    </div>
+  );
 }
